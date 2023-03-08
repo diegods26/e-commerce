de1d3d12b4499a98a78b0a7f33aa4afea0044d1f
Diego Pimenta dos Santos <diegopimenta.ds@hotmail.com>
2023-03-07 21:09:23 -0300

    End of section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -31,7 +31,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
-            //var email = User.FindFirstValue(ClaimTypes.Email);
+            
             var user = await _userManager.FindByEmailFromClaimsPrincipalAsync(User);
 
              return new UserDto
@@ -90,6 +90,12 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if(CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = 
+                    new []{"Email address is in use"}});
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
--- API/Program.cs
+++ API/Program.cs
@@ -18,6 +18,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddSwaggerDocumentation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,8 +26,7 @@ app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
-app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerDocumentation();
 
 app.UseStaticFiles();
 
